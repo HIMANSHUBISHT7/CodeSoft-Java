487137401a820d5b30c7159259f30f00fd814450
HIMANSHUBISHT7 <137715136+HIMANSHUBISHT7@users.noreply.github.com>
2024-08-31 10:44:18 +0530

    second.java

--- second.java
+++ second.java
@@ -6,35 +6,35 @@ public class second {
 
         // Get the number of subjects
         System.out.print("Enter the number of subjects: ");
-        int numSubjects = scanner.nextInt();
+        int Subjects = scanner.nextInt();
 
         // Array to store the marks for each subject
-        int[] marks = new int[numSubjects];
+        int[] marks = new int[Subjects];
         int totalMarks = 0;
 
         // Input marks for each subject
-        for (int i = 0; i < numSubjects; i++) {
+        for (int i = 0; i < Subjects; i++) {
             System.out.print("Enter marks obtained in subject " + (i + 1) + ": ");
             marks[i] = scanner.nextInt();
             totalMarks += marks[i];
         }
 
         // Calculate the average percentage
-        double averagePercentage = (double) totalMarks / numSubjects;
+        double Percentage = (double) totalMarks / Subjects;
 
         // Determine the grade based on the average percentage
         String grade;
-        if (averagePercentage >= 90) {
+        if (Percentage >= 90) {
             grade = "A+";
-        } else if (averagePercentage >= 80) {
+        } else if (Percentage >= 80) {
             grade = "A";
-        } else if (averagePercentage >= 70) {
+        } else if (Percentage >= 70) {
             grade = "B+";
-        } else if (averagePercentage >= 60) {
+        } else if (Percentage >= 60) {
             grade = "B";
-        } else if (averagePercentage >= 50) {
+        } else if (Percentage >= 50) {
             grade = "C";
-        } else if (averagePercentage >= 40) {
+        } else if (Percentage >= 40) {
             grade = "D";
         } else {
             grade = "F";
@@ -43,7 +43,7 @@ public class second {
         // Display the results
         System.out.println("\n--- Results ---");
         System.out.println("Total Marks: " + totalMarks);
-        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Average Percentage: " + Percentage + "%");
         System.out.println("Grade: " + grade);
 
         scanner.close();
